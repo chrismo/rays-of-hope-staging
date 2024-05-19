25ee6e4055bd4171d248f58a7d4be55027319f77
chrismo <chrismo@clabs.org>
2024-05-19 18:39:20 -0500

    Custom color palette added
    Used https://palettte.app/ to help build it up from a couple of purples
    grabbed off the gradient of the book cover.
    
    Re-used the tailwind config in script.js.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+tailwind.config = {
+    theme: {
+        colors: {
+            // 'book-purple': '#555A7E'
+            'book-purple': {
+                100: "#BFDEFF",
+                200: "#A8C2E5",
+                300: "#92A6CB",
+                400: "#7D8BB2",
+                500: "#697298",
+                600: "#555A7E",
+                DEFAULT: "#555A7E",
+                700: "#404162",
+                800: "#2F2C45",
+                900: "#1C1929",
+            }
+        },
+    },
+}
